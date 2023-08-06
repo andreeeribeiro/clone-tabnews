3bba56f6695ec0db9714bea1cd18f6fc307027c9
André Ribeiro <andreeeribeiro1@gmail.com>
2023-08-06 21:48:49 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto de desenvolvimento web que tem como objetivo aprimorar habilidades de programação através da clonagem do site www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Queridinha, vá até Cânticos 4:7! 😇
-    </h1>
-  );
+  return <h1>Queridinha, vá até Cânticos 4:7! 😇</h1>;
 }
 
 export default Home;
